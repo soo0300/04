e6cb100d99272f2a0378d5bc2a465e46f7bbc65b
soo0300 <soo0300@naver.com>
2022-09-28 16:49:42 +0900

    실습5

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc, char *argv[]){
 
-	int year;
-	printf("input the year: ");
-	scanf("%i",&year);
+int main(int argc, char *argv[]){	
+	unsigned int num;
+	int b;
 	
-	printf("%i", (year%4==0 && year%100!=0) || year%400==0);
-	 
+	printf("input a number: ");
+	scanf("%ui",&num);
+	
+	
+	for(b=0; num!=0; num>>=1){
+		if(num&1){
+			b++;		
+		}
+	}
+	printf("The result is: %i\n",b);
+	return 0;
+
+	
 }
-
-
